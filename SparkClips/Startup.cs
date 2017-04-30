6f44fd4d5c9daadecb427ab0e30d66cf46acf990
Luis Naranjo <luisnaranjo733@gmail.com>
2017-04-30 14:23:08 -0700

    add more relaxed password requirements

--- SparkClips/Startup.cs
+++ SparkClips/Startup.cs
@@ -51,6 +51,28 @@ namespace SparkClips
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                
+                // Cookie settings
+                options.Cookies.ApplicationCookie.ExpireTimeSpan = TimeSpan.FromDays(150);
+                options.Cookies.ApplicationCookie.LoginPath = "/Account/LogIn";
+                options.Cookies.ApplicationCookie.LogoutPath = "/Account/LogOff";
+                
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
             services.AddMvc();
 
             // Add application services.
