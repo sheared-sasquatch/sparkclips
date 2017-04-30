096a58c0563f1f08423581e363962f3e757d8526
Luis Naranjo <luisnaranjo733@gmail.com>
2017-04-30 13:02:46 -0700

    add models for gallery, image, and many to many join table

---
+++ SparkClips/Models/HairyDatabase/Models.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkClips.Models.HairyDatabase
+{
+    public class Image
+    {
+        public int ImageID { get; set; }
+        public string Filename { get; set; }
+        public Guid Guid { get; set; }
+        public string Url { get; set; }
+        // public Container Container { get; set; }
+
+        public List<GalleryEntry> GalleryEntries { get; set; } // Collection navigation property
+    }
+
+    public class GalleryEntry
+    {
+        public int GalleryEntryID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int Upvotes { get; set; }
+        public int Downvotes { get; set; }
+
+        public List<Image> Images { get; set; } // Collection navigation property
+    }
+
+    /// <summary>
+    /// Associative entity table for many to many relationship
+    /// between Gallery and Image.
+    /// </summary>
+    public class GalleryEntry_Image
+    {
+        public int ImageID { get; set; }
+        public Image Image { get; set; }
+
+        public int GalleryEntryID { get; set; }
+        public GalleryEntry GalleryEntry { get; set; }
+    }
+}
