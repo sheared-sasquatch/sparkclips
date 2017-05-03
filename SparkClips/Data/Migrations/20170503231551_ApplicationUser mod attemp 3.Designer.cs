c4713ebcf19e44cd7b094bb832665202c76f1a51
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-03 16:19:00 -0700

    it worked! we now have a custom field in our user table

--- SparkClips/Data/Migrations/20170503231551_ApplicationUser mod attemp 3.Designer.cs
+++ SparkClips/Data/Migrations/20170503231551_ApplicationUser mod attemp 3.Designer.cs
@@ -8,9 +8,10 @@ using SparkClips.Data;
 namespace SparkClips.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170503231551_ApplicationUser mod attemp 3")]
+    partial class ApplicationUsermodattemp3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
