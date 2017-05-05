97739fc17de80b406901f18e2d64044d78b9563a
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-04 17:26:51 -0700

    provide boilerplate for unit tests

---
+++ SparkClips.Tests/UploadControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Moq;
+using sparkclips.Controllers;
+using SparkClips.Data;
+using SparkClips.Models;
+using SparkClips.Services.BlobBob;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SparkClips.Tests
+{
+    public class UploadControllerTests
+    {
+
+        public async Task Index()
+        {
+            // arrange
+            var mockFileStorage = new Mock<IFileStorage>();
+            var applicationDbContext = new Mock<ApplicationDbContext>();
+
+            var controller = new UploadFilesController(mockFileStorage.Object, applicationDbContext.Object);
+
+            // act
+            var result = controller.Post();
+
+            // assert
+        }
+
+    }
+}
