40fdd2296cd17ffa0e9498abb3b1073890d8b204
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-08 09:25:42 -0700

    add initial version of unit test

--- SparkClips.Tests/UploadControllerTests.cs
+++ SparkClips.Tests/UploadControllerTests.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using sparkclips.Controllers;
 using SparkClips.Data;
 using SparkClips.Models;
+using SparkClips.Models.HairyDatabase;
 using SparkClips.Services.BlobBob;
 using System;
 using System.Collections.Generic;
@@ -14,19 +18,36 @@ namespace SparkClips.Tests
 {
     public class UploadControllerTests
     {
-
+        [Fact]
         public async Task Index()
         {
             // arrange
             var mockFileStorage = new Mock<IFileStorage>();
-            var applicationDbContext = new Mock<ApplicationDbContext>();
+            mockFileStorage
+                .Setup(x => x.UploadImage(It.IsAny<ContainerName>(), It.IsAny<FormFile>()))
+                .ReturnsAsync(new Image {
+                    Url = "link.com",
+                    Guid = Guid.NewGuid(),
+                    ContainerName = 0,
+                });
 
-            var controller = new UploadFilesController(mockFileStorage.Object, applicationDbContext.Object);
+            //var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            //optionsBuilder.Use
+            //var _dbContext = new ApplicationDbContext(optionsBuilder.Options);
+
+            var mockApplicationDbContext = new Mock<ApplicationDbContext>();
+
+            var controller = new UploadFilesController(mockFileStorage.Object, mockApplicationDbContext.Object);
+
+            var formFiles = new List<IFormFile>();
 
             // act
-            var result = controller.Post();
+            var result = await controller.Post(formFiles);
 
             // assert
+            mockApplicationDbContext.Verify(
+                foo => foo.Images.Add(It.IsAny<Image>()), Times.AtLeastOnce()
+                );
         }
 
     }
