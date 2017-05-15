d466adaed969ca0592944cbc995b2950c61f36ad
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-15 11:55:57 -0700

    scaffold controllers for tag model

---
+++ SparkClips/Controllers/ModelControllers/TagsController.cs
@@ -0,0 +1,153 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SparkClips.Data;
+using SparkClips.Models.HairyDatabase;
+
+namespace SparkClips.Controllers
+{
+    public class TagsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TagsController(ApplicationDbContext context)
+        {
+            _context = context;    
+        }
+
+        // GET: Tags
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tags.ToListAsync());
+        }
+
+        // GET: Tags/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tag = await _context.Tags
+                .SingleOrDefaultAsync(m => m.TagID == id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return View(tag);
+        }
+
+        // GET: Tags/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tags/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TagID,Name")] Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tag);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(tag);
+        }
+
+        // GET: Tags/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tag = await _context.Tags.SingleOrDefaultAsync(m => m.TagID == id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+            return View(tag);
+        }
+
+        // POST: Tags/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("TagID,Name")] Tag tag)
+        {
+            if (id != tag.TagID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tag);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TagExists(tag.TagID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(tag);
+        }
+
+        // GET: Tags/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tag = await _context.Tags
+                .SingleOrDefaultAsync(m => m.TagID == id);
+            if (tag == null)
+            {
+                return NotFound();
+            }
+
+            return View(tag);
+        }
+
+        // POST: Tags/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tag = await _context.Tags.SingleOrDefaultAsync(m => m.TagID == id);
+            _context.Tags.Remove(tag);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool TagExists(int id)
+        {
+            return _context.Tags.Any(e => e.TagID == id);
+        }
+    }
+}
