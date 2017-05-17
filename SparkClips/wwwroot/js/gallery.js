27499b749d715d1390c75e73a596a0c711fe75c3
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-17 11:20:10 -0700

    moved js to js file, carry on

---
+++ SparkClips/wwwroot/js/gallery.js
@@ -0,0 +1,14 @@
+﻿var buttons = document.querySelectorAll(".glyphicon-star");
+buttons.forEach(function (cur) {
+    cur.onclick = function () {
+        var req = new XMLHttpRequest();
+        req.open("POST", "api/GalleryEntry_ApplicationUserApi");
+        req.setRequestHeader("Content-Type", "application/json");
+        req.onload = function () { console.log("lol") };
+        req.onerror = function () { console.log("naw") };
+        console.log(this.id);
+        var json = { "GalleryEntryID": parseInt(this.id), "ApplicationUserId": 1 };
+        console.log(JSON.stringify(json));
+        req.send(json);
+    }
+});
