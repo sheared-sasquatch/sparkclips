b6d66730a9dc617bcf6a99fd2f41bfb2b5ba65e0
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-28 12:08:54 -0700

    add new log entries to database from form

--- SparkClips/Controllers/SparkClipsControllers/LogController.cs
+++ SparkClips/Controllers/SparkClipsControllers/LogController.cs
@@ -10,18 +10,24 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using SparkClips.Models;
 
 namespace SparkClips.Controllers
 {
     public class LogController : Controller
     {
         private IFileStorage _fileStorage;
+        private readonly UserManager<ApplicationUser> _userManager;
         private ILogRepository _logRepository;
+        private ApplicationDbContext _sparkClipsContext;
 
-        public LogController(IFileStorage fileStorage, ILogRepository logRepository)
+        public LogController(ApplicationDbContext sparkClipsContext, IFileStorage fileStorage, ILogRepository logRepository, UserManager<ApplicationUser> userManager)
         {
+            _sparkClipsContext = sparkClipsContext;
             _fileStorage = fileStorage; // store a reference to the file storage object from ASP .NET's dependency injection container
             _logRepository = logRepository;
+            _userManager = userManager;
         }
 
         // GET: /Log/
@@ -63,26 +69,34 @@ namespace SparkClips.Controllers
         /// <returns>A JSON object which gives some useless data about the files uploaded.
         /// This should probably become a redirect at some point.</returns>
         [HttpPost]
-        public async Task<IActionResult> ProcessEntry(List<IFormFile> files, LogEntryViewModel logEntryViewModel)
+        public async Task<IActionResult> ProcessEntry(List<IFormFile> files, LogEntry logEntry)
         {
             long size = files.Sum(f => f.Length);
+            logEntry.ApplicationUser = await _userManager.GetUserAsync(User);
+            _sparkClipsContext.LogEntries.Add(logEntry);
+            _sparkClipsContext.SaveChanges();
 
             foreach (FormFile formFile in files)
             {
                 if (formFile.Length > 0)
                 {
                     Image image = await _fileStorage.UploadImage(ContainerName.Gallery, formFile);
-                    Debug.WriteLine(image.Url);
-                    Debug.WriteLine(image.Guid);
-                    //_sparkClipsContext.Images.Add(image);
-                    //_sparkClipsContext.SaveChanges();
-                    Debug.WriteLine("ID: " + image.ImageID);
+                    _sparkClipsContext.Images.Add(image);
+                    _sparkClipsContext.SaveChanges();
+
+                    LogEntry_Image associative_entity = new LogEntry_Image {
+                      LogEntryID = logEntry.LogEntryID,
+                      ImageID = image.ImageID
+                    };
+                    _sparkClipsContext.LogEntry_Image.Add(associative_entity);
+                    _sparkClipsContext.SaveChanges();
                 }
             }
 
             // process uploaded files
             // Don't rely on or trust the FileName property without validation.
 
+            return RedirectToAction("Index");
             return Ok(new { count = files.Count, size });
         }
     }
